3a6f41216abedab12510cc39ed0cd9199b12142f
Swapnamoy Gope <gopeswapnamoy.biz@gmail.com>
2026-07-26 10:10:15 +0530

    Add files via upload

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { projects, skillCategories, timelineEvents } from "../data/portfolio";
 
